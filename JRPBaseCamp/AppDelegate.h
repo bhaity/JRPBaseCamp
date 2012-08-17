7bfc601cb4286c6ff16f8185dfa966d2b94f03c6
bhaity <bhaity@gmail.com>
2012-08-17 17:11:18 -0400

    Initial Commit

---
+++ JRPBaseCamp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JRPBaseCamp
+//
+//  Created by Sikandar Shukla on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
